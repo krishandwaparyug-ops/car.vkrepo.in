1fbb10f73076b0d8b3df8f9e9c36f32b1e5197a9
sks64 <singhiiit6@gmail.com>
2026-02-24 19:06:39 +0530

    Update app.config.js

--- vers_web_client-main/vers_web_client-main/src/configs/app.config.js
+++ vers_web_client-main/vers_web_client-main/src/configs/app.config.js
@@ -1,5 +1,5 @@
 const appConfig = {
-    apiPrefix: 'https://kartika.onrender.com/api/',
+     apiPrefix: 'https://kartika.onrender.com/api/',
     //apiPrefix: 'http://localhost:5002/api/',
     //apiPrefix: 'https://api.vkrepo.in/api/',
     webhookPrefix: 'https://webhooks.vkrepo.in/webhooks/',
