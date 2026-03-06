257479c8d18a265c5c2f7b930424d23a7d2a1e08
Saurav Kumar <singhiiit6@gmail.com>
2026-03-06 13:34:43 +0530

    latest1

--- vers_web_client-main/vers_web_client-main/src/configs/app.config.js
+++ vers_web_client-main/vers_web_client-main/src/configs/app.config.js
@@ -2,7 +2,7 @@ const appConfig = {
      //apiPrefix: 'https://kartika.onrender.com/api/',
     //apiPrefix: 'http://localhost:5002/api/',
     //apiPrefix: 'https://api.vkrepo.in/api/',
-    apiPrefix: 'https://stingray-app-4e5cp.ondigitalocean.app/',
+    apiPrefix: 'https://stingray-app-4e5cp.ondigitalocean.app/api/',
     webhookPrefix: 'https://webhooks.vkrepo.in/webhooks/',
     authenticatedEntryPath: '/dashboard',
     unAuthenticatedEntryPath: '/sign-in',
